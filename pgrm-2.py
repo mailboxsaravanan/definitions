46dd60ab94cc85314cc46efef656549d3ed77f05
mailboxsaravanan <83913718+mailboxsaravanan@users.noreply.github.com>
2021-05-27 13:38:45 -0400

    Add files via upload

---
+++ pgrm-2.py
@@ -0,0 +1,10 @@
+#calculate area of cone : 1/3 pi r2 h 
+#Assign data value to variable
+radius = 20  
+height = 10.5
+
+#formula for Perimeter of circle 2Pir
+area_of_cone = 1/3 * 3.14 * radius**2 * height
+    
+print(area_of_cone)
+    
