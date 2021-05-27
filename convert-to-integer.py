ffd65cd52f633ff2b2b5d1a8ad0f7484d1139bf0
mailboxsaravanan <83913718+mailboxsaravanan@users.noreply.github.com>
2021-05-27 13:56:22 -0400

    Add files via upload

---
+++ convert-to-integer.py
@@ -0,0 +1,12 @@
+#calculate area of cone : 1/3 pi r2 h 
+#Assign data value to variable
+radius = 20  
+height = 10.5
+
+#formula for Perimeter of circle 2Pir
+area_of_cone = 1/3 * 3.14 * radius**2 * height
+
+#convert in full number / interger
+area_of_cone_in_int = int(area_of_cone)
+print(area_of_cone_in_int)
+    
