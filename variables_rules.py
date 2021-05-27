d5369c1d192f1570024d3e7bcf020222a46d1218
mailboxsaravanan <83913718+mailboxsaravanan@users.noreply.github.com>
2021-05-27 14:24:49 -0400

    Add files via upload

---
+++ variables_rules.py
@@ -0,0 +1,7 @@
+##rules to create variables
+#can start with alphabets
+#can start with underscore
+#can't start with numeric
+#can be alpha numeric
+#case sensitive , so we can use both cases as separate values
+    
