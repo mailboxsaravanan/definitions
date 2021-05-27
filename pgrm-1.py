54aa994e7320f53d62c010d75241425e0c6163cf
mailboxsaravanan <83913718+mailboxsaravanan@users.noreply.github.com>
2021-05-27 13:14:07 -0400

    Add files via upload

---
+++ pgrm-1.py
@@ -0,0 +1,9 @@
+#calculate perimeter of circle : 2 pi radius
+#Assign data value to variable
+radius = 20   
+    
+#formula for Perimeter of circle 2Pir
+Perimeter_of_circle = 2 * 3.14 * radius
+    
+print(Perimeter_of_circle)
+    
