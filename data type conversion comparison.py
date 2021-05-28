b19d172971e6fdda7ab422c462bf9b8d5e508fd5
mailboxsaravanan <83913718+mailboxsaravanan@users.noreply.github.com>
2021-05-29 00:19:57 +0530

    Add files via upload

---
+++ data type conversion comparison.py
@@ -0,0 +1,109 @@
+
+
+#Int to float conversation
+
+A = 10
+B = float(A)
+print (A)
+print (type(A))
+print (B)
+print (type(B))
+
+#Int to bool conversation
+
+A = 10
+B = bool(A)
+print (A)
+print (type(A))
+print (B)
+print (type(B))
+
+#Int to str conversation
+
+A = 10
+B = str(A)
+print (A)
+print (type(A))
+print (B)
+print (type(B))
+
+#float to int conversation
+
+A = 10.55
+B = int(A)
+print (A)
+print (type(A))
+print (B)
+print (type(B))
+
+#float to bool conversation
+
+A = 10.55
+B = bool(A)
+print (A)
+print (type(A))
+print (B)
+print (type(B))
+
+#float to str conversation
+
+A = 10.55
+B = str(A)
+print (A)
+print (type(A))
+print (B)
+print (type(B))
+
+#boolean to int conversation
+
+A = True
+B = int(A)
+print (A)
+print (type(A))
+print (B)
+print (type(B))
+
+#boolean to float conversation
+
+A = False
+B = float(A)
+print (A)
+print (type(A))
+print (B)
+print (type(B))
+
+#boolean to str conversation
+
+A = True
+B = str(A)
+print (A)
+print (type(A))
+print (B)
+print (type(B))
+
+#String to int conversation / cant convert
+
+#A = "True"
+#B = int(A)
+#print (A)
+#print (type(A))
+#print (B)
+#print (type(B))
+
+#String to float conversation / cant convert
+
+#A = "False"
+#B = float(A)
+#print (A)
+#print (type(A))
+#print (B)
+#print (type(B))
+
+#String to boolean conversation
+
+A = "True"
+B = bool(A)
+print (A)
+print (type(A))
+print (B)
+print (type(B))
