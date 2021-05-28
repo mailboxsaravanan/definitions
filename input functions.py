82ec013db0584e370a590201a23cd1aafdf76c6e
mailboxsaravanan <83913718+mailboxsaravanan@users.noreply.github.com>
2021-05-29 00:23:38 +0530

    Add files via upload

---
+++ input functions.py
@@ -0,0 +1,23 @@
+#task 2
+#perimeter with input from user
+Radius = float(input ("Enter radius :"))
+#if input function defined as int and given input is float , wont accept
+# but vice versa accepted
+Perimeter = (2 * 3.14 * Radius**2)
+print(Perimeter)
+
+print("#####################################")
+#task 3
+#perimeter with input from user
+R = float(input ("Enter Radius :"))
+H = int(input ("Enter Height :"))
+#if input function defined as int and given input is float , wont accept
+#but vice versa accepted
+             
+Area_Cone = (1/3 * 3.14 * R**2 * H)
+print(Area_Cone)
+
+
+
+
+
