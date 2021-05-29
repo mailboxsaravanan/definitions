ff63be92cfea859d58e5e893d01b27b8ed146680
mailboxsaravanan <83913718+mailboxsaravanan@users.noreply.github.com>
2021-05-29 22:25:57 +0530

    indexing&slicing

---
+++ indexing&slicing.py
@@ -0,0 +1,17 @@
+#indexing
+a = input("get string: ")
+print(a)
+print(a[0])
+print(a[-1])
+print(a[len(a)//2])
+print(a[len(a)//2 -1])
+print(a[-3])
+print(a[len(a)//2 +1])
+#slicing
+print(a[-5:5])
+print(a[1:-1])
+print(a[:2])
+print(a[:])
+#indexing interval
+print(a[2:5:1])
+print(a[-6:-1:3])
