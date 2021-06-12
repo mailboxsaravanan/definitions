8658bba91f249a38727f74b90ad2c343a953c615
mailboxsaravanan <83913718+mailboxsaravanan@users.noreply.github.com>
2021-06-12 12:33:17 -0400

    Sets-identifier-tasks

---
+++ sets-identifier.py
@@ -0,0 +1,36 @@
+
+
+sets1 = {"name": "John","age": 30,"married": True,"divorced": False,"children": ("Ann","Billy"),"pets": None,"cars": [{"model1": "BMW 230", "mpg1": 27.5},{"model2": "Ford Edge", "mpg2": 24.1}]}
+
+#print Billy
+#print model2 name
+#print model1 mpg1
+#print divorced satatus
+#print age
+
+
+print(sets1)
+print(type(sets1))
+
+print (sets1["children"][1])
+print (sets1["cars"][1]["model2"])
+print (sets1["cars"][0]["mpg1"])
+print (sets1["divorced"])
+print (sets1["age"])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
